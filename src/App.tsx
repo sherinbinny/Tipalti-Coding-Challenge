0f7ae86d5b22398a767fc345b0ebf0b198737344
Rory Flint <rory.flint@tipalti.com>
2024-10-11 14:08:16 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 function App() {
   return (
     <div id="template-text">
-      <h1>React Starter Template - TypeScript</h1>
-      <p>
-        For JavaScript please use{" "}
-        <a href="https://github.com/ruairidhflint/react-template">this</a>{" "}
-        template
-      </p>
+      <h1>React Starter Template</h1>
+      <p>A lightweight React starter template in TypeScript and Vite</p>
     </div>
   );
 }
